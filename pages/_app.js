184c962513f09ed77825be56efcaec3e3d8bc0b8
Tom Moore <tom_moore95@hotmail.co.uk>
2021-09-15 16:27:08 +0100

    Updated favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
 import "../styles/globals.css";
 import { CvContextProvider } from "../store/cv-context";
+import React from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <CvContextProvider>
-      <Component {...pageProps} />
-    </CvContextProvider>
+    <React.Fragment>
+      <Head>
+        <title>CV Builder</title>
+        <link
+          rel="shortcut icon"
+          type="image/png"
+          href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/facebook/65/page-facing-up_1f4c4.png"
+        ></link>
+      </Head>
+      <CvContextProvider>
+        <Component {...pageProps} />
+      </CvContextProvider>
+    </React.Fragment>
   );
 }
 
